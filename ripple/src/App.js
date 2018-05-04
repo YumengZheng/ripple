39122344446cb8fd8ab3ced905a6f0a6bb4b72b6
ivy <zhengyumeng1125@gmail.com>
2018-05-04 00:25:31 -0700

    ripple finished `

--- ripple/src/App.js
+++ ripple/src/App.js
@@ -1,18 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      circleClass: '',
+      x: 0,
+      y: 0
+    }
+    this.handleTouchStart = this.handleTouchStart.bind(this)
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleLeave = this.handleLeave.bind(this)
+    this.handleDrag = this.handleDrag.bind(this)
+    this.handleTouchMove = this.handleTouchMove.bind(this)
+  }
+  handleTouchStart(e){
+    this.setState({
+      circleClass: 'circle',
+      x: e.touches[0].pageX-30,
+      y: e.touches[0].pageY-40
+    })
+  }
+  handleMouseDown(e){
+    this.setState({
+      circleClass: 'circle',
+      x: e.pageX-30,
+      y: e.pageY-30
+    })
+  }
+  handleTouchMove(e){
+    this.setState({
+      circleClass: 'circle circle-drag',
+      x: e.touches[0].pageX-30,
+      y: e.touches[0].pageY-40
+    })
+  }
+  handleDrag(e){
+    this.setState({
+      circleClass: 'circle circle-drag',
+      x: e.pageX-30,
+      y: e.pageY-30
+    })
+  }
+  handleLeave(){
+    this.setState({
+      circleClass: ''
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="screen"                
+             onTouchStart={this.handleTouchStart} 
+             onTouchEnd={this.handleLeave}
+             onTouchMove={this.handleTouchMove}
+             onMouseDown={this.handleMouseDown}
+             onMouseUp={this.handleLeave}>
+          <div className='dragging-box' 
+               draggable="true" 
+               onDrag={this.handleDrag}
+               onDragEnd={this.handleLeave}></div>
+          <div id="circle" 
+               className={this.state.circleClass} 
+               style = {{marginLeft: this.state.x, marginTop: this.state.y}}></div>
+        </div>
       </div>
     );
   }
